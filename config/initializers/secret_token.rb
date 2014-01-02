2b9255f1ab92ab3343a71302d83ee04b0ab60967
Ravit Bar <ravit.b@fiverr.com>
2014-01-02 12:04:56 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyDemo::Application.config.secret_key_base = '5863c306e5cba1519155435209757c5aef0b12f17f8969dd3f2f328d70798b14cf7d8dc51e505a9f618a5872aa6133f77dc6f824204c122c9df0ca7852845a22'
